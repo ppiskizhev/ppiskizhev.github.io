56e767ea5b47f8db2f99696b3936e78871e46651
Peter Piskizhev <p.piskizhev@gmail.com>
2018-06-16 14:58:37 +0300

    c

--- touchevent/js.js
+++ touchevent/js.js
@@ -7,11 +7,28 @@ obj.addEventListener('touchmove', dragMove);
 function dragStart(e) {
   shiftX = e.changedTouches[0].pageX - getCoords(this).left;
   shiftY = e.changedTouches[0].pageY - getCoords(this).top;
+  e.preventDefault();
 }
 
 function dragMove(e) {
   this.style.left = e.changedTouches[0].pageX - shiftX + 'px';
   this.style.top = e.changedTouches[0].pageY - shiftY + 'px';
+  e.preventDefault();
+}
+
+obj.onmousedown = function(e) {
+  shiftX = e.pageX - getCoords(this).left;
+  shiftY = e.pageY - getCoords(this).top;
+
+  document.onmousemove = function(e) {
+    obj.style.left = e.pageX - shiftX + 'px';
+    obj.style.top = e.pageY - shiftY + 'px';
+  }
+
+  document.onmouseup = function(e) {
+    document.onmousemove = document.onmouseup = null;
+  }
+  
 }
     
 function getCoords(elem) {
